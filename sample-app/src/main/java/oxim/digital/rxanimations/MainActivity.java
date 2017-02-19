80206f22531b173787b8017c572154a949d460e8
MihaelFrancekovic <mihael.francekovic@gmail.com>
2017-02-19 21:13:05 +0100

    Module rename

--- sample-app/src/main/java/oxim/digital/rxanimations/MainActivity.java
+++ sample-app/src/main/java/oxim/digital/rxanimations/MainActivity.java
@@ -1,14 +1,18 @@
 package oxim.digital.rxanimations;
 
-import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import oxim.digital.rx2anim.RxValueAnimator;
+import io.reactivex.Completable;
+import io.reactivex.CompletableEmitter;
+import io.reactivex.CompletableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,10 +30,26 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.root_view)
     public void animateSampleView() {
-        final ValueAnimator opacityAnimator = ValueAnimator.ofFloat(0.f, 1.f);
-        opacityAnimator.setDuration(ANIMATION_DURATION);
-        final RxValueAnimator rxValueAnimator = RxValueAnimator.from(opacityAnimator,
-                animator -> sampleView.setAlpha((float) animator.getAnimatedValue()));
-        rxValueAnimator.subscribe();
+        final Completable logJob = Completable.create(new LogCompletable());
+
+        Log.i("MAIN", "Waiting");
+
+        logJob.subscribeOn(Schedulers.computation())
+              .observeOn(AndroidSchedulers.mainThread())
+              .subscribe(() -> Log.i("MAIN", "Done"), Throwable::printStackTrace);
+    }
+
+    private static final class LogCompletable implements CompletableOnSubscribe {
+
+        @Override
+        public void subscribe(final CompletableEmitter emitter) throws Exception {
+            if (!emitter.isDisposed()) {
+                Log.i("CLAZZ", "Emitter is disposed");
+                return;
+            }
+
+            Log.i("CLAZZ", "I am done");
+            emitter.onComplete();
+        }
     }
 }
