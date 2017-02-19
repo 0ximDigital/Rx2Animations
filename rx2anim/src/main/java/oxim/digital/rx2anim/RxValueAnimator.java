a20be389312cea6d0582be2237b3cbbe61dbc351
MihaelFrancekovic <mihael.francekovic@gmail.com>
2017-02-20 00:15:07 +0100

    RxValueAnimator

--- rx2anim/src/main/java/oxim/digital/rx2anim/RxValueAnimator.java
+++ rx2anim/src/main/java/oxim/digital/rx2anim/RxValueAnimator.java
@@ -3,6 +3,7 @@ package oxim.digital.rx2anim;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
+import android.animation.ValueAnimator.AnimatorUpdateListener;
 
 import io.reactivex.Completable;
 import io.reactivex.CompletableObserver;
@@ -13,62 +14,119 @@ import static io.reactivex.android.MainThreadDisposable.verifyMainThread;
 
 public final class RxValueAnimator extends Completable {
 
+    private static final float FULLY_ANIMATED = 1.0f;
+
     private final ValueAnimator valueAnimator;
     private final Consumer<ValueAnimator> valueUpdateAction;
+    private final Consumer<ValueAnimator> animationCancelAction;
 
     public static RxValueAnimator from(final ValueAnimator valueAnimator, final Consumer<ValueAnimator> valueUpdateAction) {
-        return new RxValueAnimator(valueAnimator, valueUpdateAction);
+        return from(valueAnimator, valueUpdateAction, aValueAnimator -> {});
     }
 
-    private RxValueAnimator(final ValueAnimator valueAnimator, final Consumer<ValueAnimator> valueUpdateAction) {
+    public static RxValueAnimator from(final ValueAnimator valueAnimator, final Consumer<ValueAnimator> valueUpdateAction,
+                                       final Consumer<ValueAnimator> animationCancelAction) {
+        return new RxValueAnimator(valueAnimator, valueUpdateAction, animationCancelAction);
+    }
+
+    private RxValueAnimator(final ValueAnimator valueAnimator, final Consumer<ValueAnimator> valueUpdateAction,
+                            final Consumer<ValueAnimator> animationCancelAction) {
         this.valueAnimator = valueAnimator;
         this.valueUpdateAction = valueUpdateAction;
+        this.animationCancelAction = animationCancelAction;
     }
 
     @Override
-    protected void subscribeActual(CompletableObserver completableObserver) {
+    protected void subscribeActual(final CompletableObserver completableObserver) {
         verifyMainThread();
-        Listener listener = new Listener(completableObserver, valueAnimator, valueUpdateAction);
-        completableObserver.onSubscribe(listener);
-        valueAnimator.addUpdateListener(listener);
-        valueAnimator.addListener(listener.animatorListener);
+        final UpdateListener updateListener = new UpdateListener(completableObserver, valueUpdateAction);
+        final AnimationEndListener endListener = new AnimationEndListener(completableObserver, valueAnimator, animationCancelAction);
+        final ValueAnimatorDisposable animatorDisposable = new ValueAnimatorDisposable(valueAnimator, updateListener, endListener);
+        completableObserver.onSubscribe(animatorDisposable);
+
+        valueAnimator.addUpdateListener(updateListener);
+        valueAnimator.addListener(endListener);
         valueAnimator.start();
     }
 
-    private class Listener extends MainThreadDisposable implements ValueAnimator.AnimatorUpdateListener {
-        private final CompletableObserver observer;
-        private final ValueAnimator animator;
-        private final Consumer<ValueAnimator> valueUpdateAction;
-        private final AnimatorListenerAdapter animatorListener = new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(final Animator animation) {
-                animator.removeListener(this);
-                observer.onComplete();
-            }
-        };
+    private static final class ValueAnimatorDisposable extends MainThreadDisposable {
 
-        private Listener(CompletableObserver observer, ValueAnimator valueAnimator, Consumer<ValueAnimator> valueUpdateAction) {
-            this.observer = observer;
-            this.animator = valueAnimator;
-            this.valueUpdateAction = valueUpdateAction;
+        private final ValueAnimator animator;
+
+        private final UpdateListener updateListener;
+        public final AnimationEndListener animationEndListener;
+
+        public ValueAnimatorDisposable(final ValueAnimator animator, final UpdateListener updateListener,
+                                       final AnimationEndListener animationEndListener) {
+            this.animator = animator;
+            this.updateListener = updateListener;
+            this.animationEndListener = animationEndListener;
         }
 
         @Override
         protected void onDispose() {
-            animator.removeUpdateListener(this);
-            animator.removeListener(animatorListener);
-            animator.end();
+            if (animator.getAnimatedFraction() != FULLY_ANIMATED) {
+                animator.cancel();
+            } else {
+                animator.end();
+            }
+            animator.removeUpdateListener(updateListener);
+            animator.removeListener(animationEndListener);
+        }
+    }
+
+    private static final class UpdateListener implements AnimatorUpdateListener {
+
+        private final CompletableObserver observer;
+        private final Consumer<ValueAnimator> valueUpdateAction;
+
+        public UpdateListener(final CompletableObserver observer, final Consumer<ValueAnimator> valueUpdateAction) {
+            this.observer = observer;
+            this.valueUpdateAction = valueUpdateAction;
         }
 
         @Override
-        public void onAnimationUpdate(ValueAnimator animation) {
-            if (!isDisposed()) {
-                try {
-                    valueUpdateAction.accept(animation);
-                } catch (Exception e) {
-                    observer.onError(e);
-                }
+        public void onAnimationUpdate(final ValueAnimator animator) {
+            try {
+                valueUpdateAction.accept(animator);
+            } catch (Exception e) {
+                observer.onError(e);
             }
+        }
+    }
+
+    private static final class AnimationEndListener extends AnimatorListenerAdapter {
+
+        private final CompletableObserver observer;
+        private final ValueAnimator animator;
+        private final Consumer<ValueAnimator> animationCancelAction;
+
+        public AnimationEndListener(final CompletableObserver observer, final ValueAnimator animator,
+                                    final Consumer<ValueAnimator> animationCancelAction) {
+            this.observer = observer;
+            this.animator = animator;
+            this.animationCancelAction = animationCancelAction;
+        }
+
+        @Override
+        public void onAnimationCancel(final Animator animation) {
+            try {
+                animationCancelAction.accept(animator);
+                dispose();
+            } catch (Exception e) {
+                animator.removeListener(this);
+                observer.onError(e);
+            }
+        }
+
+        @Override
+        public void onAnimationEnd(final Animator animation) {
+            dispose();
+        }
+
+        private void dispose() {
+            animator.removeListener(this);
+            observer.onComplete();
         }
     }
 }
--- sample-app/src/main/java/oxim/digital/rxanimations/MainActivity.java
+++ sample-app/src/main/java/oxim/digital/rxanimations/MainActivity.java
@@ -1,17 +1,22 @@
 package oxim.digital.rxanimations;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+
+import java.util.concurrent.TimeUnit;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Completable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.disposables.Disposables;
+import oxim.digital.rx2anim.RxValueAnimator;
 
-import static oxim.digital.rx2anim.RxAnimations.fadeOut;
 import static oxim.digital.rx2anim.RxAnimations.hideViewGroupChildren;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,7 +46,15 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.root_view)
     public void animateSampleView() {
-        fadeOut(topView, 1000, 500)
-                .subscribe();
+        final ValueAnimator valueAnimator = ValueAnimator.ofFloat(1.f, 0.f);
+        valueAnimator.setDuration(1000)
+                     .setInterpolator(new LinearInterpolator());
+
+        final Disposable disposable = RxValueAnimator.from(valueAnimator, animator -> topView.setAlpha((float) animator.getAnimatedValue()),
+                                                           cancleAnimator -> topView.setAlpha(1.0f))
+                                                     .subscribe();
+
+        Completable.timer(500, TimeUnit.MILLISECONDS)
+                   .subscribe(disposable::dispose);
     }
 }
